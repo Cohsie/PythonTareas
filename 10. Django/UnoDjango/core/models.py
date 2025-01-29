57ea96be99ccbd780068eda1b66b59069563c354
Cohsie <joseantonio.torresbar@gmail.com>
2025-01-29 20:40:12 +0100

    update

--- 10. Django/UnoDjango/core/models.py
+++ 10. Django/UnoDjango/core/models.py
@@ -13,10 +13,13 @@ class Author (models.Model):
     age=models.PositiveSmallIntegerField (verbose_name='Edad',
     )
 
+    def __str__(self):
+        return f'{self.name} {self.last_name}'
+
 class Book (models.Model):
-    title=models.CharField('Teo va a picar código', 
+    title=models.CharField('Nombre del libro', 
     max_length=255, 
-    unique=True
+    unique=True #Para no introducir dos libros con el mismo título
     )
     cod=models.CharField('Código',
     max_length=15,
@@ -24,4 +27,10 @@ class Book (models.Model):
     )
     author=models.OneToOneField(Author,on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f'{self.title}'
+
 author=models.OneToOneField(Author,on_delete=models.SET_NULL,null=True)
+#Indico que en el campo autor de un libro este se ponga null si dicho campo 
+#se ha borrado
+
